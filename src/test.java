328cf5aa3b7daea7df3949ebbb3122dd2a01521e
KennysHit <1239667935@qq.com>
2020-02-09 00:03:05 +0800

    update03

--- src/test.java
+++ src/test.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        File file = new File("points.txt");
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        for (int i=0;i<5;i++){
-            System.out.println(scanner.nextInt());
-        }
+
     }
 }
